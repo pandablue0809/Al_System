7ee009571f6d1b82f61f5c7c6ceaaf4c17ded61d
pandablue0809 <bluep0415@gmail.com>
2024-03-08 12:31:19 -0800

    fix-some-auth-form

--- src/components/Input/RoleSelect.tsx
+++ src/components/Input/RoleSelect.tsx
@@ -3,11 +3,11 @@ import React, { useState } from 'react';
 export type SelectOption = {
   value: string;
   label: string;
-}
+};
 
 export type RoleSelectProps = {
   onSelectRole: (value: string) => void;
-}
+};
 
 const RoleSelect: React.FC<RoleSelectProps> = ({ onSelectRole }) => {
   const [selectedValue, setSelectedValue] = useState<string>('');
@@ -18,7 +18,7 @@ const RoleSelect: React.FC<RoleSelectProps> = ({ onSelectRole }) => {
     { value: 'Customer', label: 'Customer' },
     { value: 'Investors', label: 'Investors' },
     { value: 'Vendors', label: 'Vendors' },
-];
+  ];
 
   const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const value = event.target.value;
@@ -27,23 +27,21 @@ const RoleSelect: React.FC<RoleSelectProps> = ({ onSelectRole }) => {
   };
 
   return (
-      <div className=" m-2">
-        <div className="relative">
-          <select className={`px-4 py-2 border w-full border-gray-300 rounded-md focus:outline-none`} value={selectedValue} onChange={handleChange}>
+    <div className='m-2 mb-3'>
+      <div className='relative'>
+        <select
+          className={`px-4 py-2 border w-full border-gray-300 rounded-md focus:outline-none`}
+          value={selectedValue}
+          onChange={handleChange}>
           {options.map((option, index) => (
             <option key={index} value={option.value}>
               {option.label}
             </option>
           ))}
-          </select>
-        </div>
+        </select>
       </div>
-
+    </div>
   );
 };
 
 export default RoleSelect;
-
-
-
-  
