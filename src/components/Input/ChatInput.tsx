da52a7e53b43ea80bbc685ca431fd5093cbf4818
pandablue0809 <bluep0415@gmail.com>
2024-03-19 04:33:12 -0700

    feat/chat-room

--- src/components/Input/ChatInput.tsx
+++ src/components/Input/ChatInput.tsx
@@ -1,4 +1,7 @@
 import React, { ChangeEvent, useEffect, useState } from 'react';
+import SendIcon from '@mui/icons-material/Send';
+import KeyboardVoiceIcon from '@mui/icons-material/KeyboardVoice';
+import AttachFileIcon from '@mui/icons-material/AttachFile';
 
 export type ChatInputProps = {
   value?: string;
@@ -46,11 +49,11 @@ const ChatInput: React.FC<ChatInputProps> = ({ value, onSendMsg }) => {
   };
 
   return (
-    <div className='absolute bottom-0 w-full pt-2 md:pt-0 dark:border-white/20 md:border-transparent md:dark:border-transparent md:w-[calc(100%-.5rem)]'>
-      <div className='stretch mx-2 flex flex-row gap-3 last:mb-2 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl'>
+    <div className='absolute bottom-2 pt-2 md:pt-0 dark:border-white/20 md:border-transparent md:dark:border-transparent w-[60vw]'>
+      <div className='mx-2 flex flex-row gap-3 last:mb-2 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl'>
         <div className='relative flex h-full flex-1 flex-col'>
           <div className='flex w-full items-center'>
-            <div className='overflow-hidden [&amp;:has(textarea:focus)]:border-token-border-xheavy [&amp;:has(textarea:focus)]:shadow-[0_2px_6px_rgba(0,0,0,.05)] flex flex-col w-full flex-grow relative border dark:text-white rounded-2xl bg-token-main-surface-primary border-token-border-medium'>
+            <div className='overflow-hidden [&amp;:has(textarea:focus)]:border-token-border-xheavy [&amp;:has(textarea:focus)]:shadow-[0_2px_6px_rgba(0,0,0,.05)] flex flex-col w-full flex-grow relative border text-white rounded-2xl bg-token-main-surface-primary border-token-border-medium'>
               <textarea
                 tabIndex={0}
                 value={chatContent}
@@ -58,18 +61,26 @@ const ChatInput: React.FC<ChatInputProps> = ({ value, onSendMsg }) => {
                 onKeyDown={handleKeyDown}
                 rows={rows}
                 placeholder='Message ChatGPT…'
-                className='rounded-2xl m-0 w-full resize-none border-0 bg-transparent focus:ring-0 focus-visible:ring-0 dark:bg-transparent py-[10px] pr-10 md:py-3.5 md:pr-1 max-h-52 placeholder-black/50 dark:placeholder-white/50 pl-3 md:pl-4'></textarea>
+                className='rounded-2xl m-0 w-full resize-none border-0 bg-transparent focus:ring-0 focus-visible:ring-0 dark:bg-transparent py-[10px] pr-10 md:py-3.5 md:pr-1 max-h-52 placeholder-white/50 pl-3 md:pl-4'></textarea>
               <button
                 disabled={isEmpty}
                 onClick={handleClick}
-                className='absolute bottom-1.5 right-2 rounded-lg border border-black bg-black p-0.5 text-white transition-colors enabled:bg-black focus:border-black disabled:text-gray-400 disabled:opacity-10 dark:border-white dark:bg-white dark:hover:bg-white md:bottom-3 md:right-3'
+                className='absolute bottom-1.5 right-2 rounded-lg hover:bg-primary py-0.5 px-1 text-white transition-colors disabled:bg-inherit  disabled:text-gray-400 disabled:opacity-10  dark:bg-white dark:hover:bg-white md:bottom-3 md:right-3'
                 data-testid='send-button'>
-                <span className='' data-state='closed'>
-                  <svg width='24' height='24' viewBox='0 0 24 24' fill='none' className='text-white dark:text-black'>
-                    <path d='M7 11L12 6L17 11M12 18V7' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'></path>
-                  </svg>
-                </span>
+                <SendIcon className='text-sm' />
               </button>
+
+              <button
+                disabled={isEmpty}
+                color="white" className="md:bottom-3 bottom-1.5 right-12 text-lg p-1 rounded-lg hover:bg-primary absolute py-0.5 disabled:bg-inherit  disabled:text-gray-400 disabled:opacity-10" >
+                <KeyboardVoiceIcon />
+              </button>
+              <button
+                disabled={isEmpty}
+                color="white" className="md:bottom-3 bottom-1.5 right-20 text-lg p-1 rounded-lg hover:bg-primary absolute py-0.5 disabled:bg-inherit  disabled:text-gray-400 disabled:opacity-10" >
+                <AttachFileIcon />
+              </button>
+
             </div>
           </div>
         </div>
